6916c18c85d8411cb5dda3f0e2fc34eda8e11258
Clint <clintp1@gmail.com>
2015-09-09 16:00:26 +0100

    Refactored todos controller. test green.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,8 @@ class ApplicationController < ActionController::Base
   def sign_in_as(email)
     session[:current_email] = email
   end
+
+  def current_user
+    User.new(current_email)
+  end
 end
